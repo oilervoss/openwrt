45b4688e060db036ecbfaa17ae2546c63c0169a2
oilervoss <euler(a)alves(.)pro(.)br>
2017-10-28 20:00:48 -0200

    Added error message on adding trackers

--- addtracker.sh
+++ addtracker.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Top best public trackers: https://github.com/ngosang/trackerslist
 
 TRANSMISSION_REMOTE='/usr/bin/transmission-remote'
+
+# The following can be a command that will generate a tracker list with one tracker per line 
+# e.g. cat /some/path/trackers.txt for a static list
+
 LIVE_TRACKERS_LIST_CMD='curl -s --url https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt'
 
 
@@ -21,26 +25,33 @@ fi
 INDEX=$1
 
 if [ "${INDEX//[0-9]}" != "" ] ; then
-        TORRENT=`$TRANSMISSION_REMOTE -l|grep -i $1`
-        INDEX=`echo $TORRENT | sed -n 's/\([0-9]\+\).*/\1/p'`
+	TORRENT=`$TRANSMISSION_REMOTE -l|grep -i $1`
+	INDEX=`echo $TORRENT | sed -n 's/\([0-9]\+\).*/\1/p'`
 
-        if [ "$INDEX" != "" ] ; then
-                echo "I found the following torrent:"
-                $TRANSMISSION_REMOTE -l | sed -n 's/\(^.\{4\}\).\{64\}/\1/p' | grep -i $1
-        fi
+	if [ "$INDEX" != "" ] ; then
+		echo "I found the following torrent:"
+		$TRANSMISSION_REMOTE -l | sed -n 's/\(^.\{4\}\).\{64\}/\1/p' | grep -i $1
+	fi
 
 fi
 
 if [ "${INDEX//[0-9]}" != "" -o "$INDEX" = "" ] ; then
-        echo "I didn't find a torrent with the text: $1"
-        $TRANSMISSION_REMOTE -l | sed -n 's/\(^.\{4\}\).\{64\}/\1/p'
-        exit 1
+	echo "I didn't find a torrent with the text: $1"
+	$TRANSMISSION_REMOTE -l | sed -n 's/\(^.\{4\}\).\{64\}/\1/p'
+	exit 1
 fi
+
+echo
 
 $LIVE_TRACKERS_LIST_CMD | while read TRACKER
 do
-        if [ "$TRACKER" != "" ]; then
-                echo "Adding $TRACKER"
-                $TRANSMISSION_REMOTE -t $INDEX -td $TRACKER
-        fi
+	if [ "$TRACKER" != "" ]; then
+		echo -n "Adding $TRACKER"
+		$TRANSMISSION_REMOTE -t $INDEX -td $TRACKER 1>/dev/null 2>&1 
+		if [ $? -eq 0 ]; then
+			echo " --> Success! "
+		else
+			echo "  < Failed > "
+		fi
+	fi
 done
